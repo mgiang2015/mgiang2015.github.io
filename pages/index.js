96ac93c55e1413a5dc540e0b02e800b9111f1678
mgiang2015 <mgiang2015@gmail.com>
2023-07-03 16:27:27 +0800

    Remove getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import ContainerBlock from "../components/ContainerBlock";
-import FavouriteProjects from "../components/FavouriteProjects";
-import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
 import getLatestRepos from "@lib/getLatestRepos";
 import userData from "@constants/data";
@@ -21,7 +17,7 @@ export default function Home({ repositories }) {
     </ContainerBlock>
   );
 }
-
+/*
 export const getServerSideProps = async () => {
   console.log(process.env.GITHUB_AUTH_TOKEN);
   let token = process.env.GITHUB_AUTH_TOKEN;
@@ -35,3 +31,4 @@ export const getServerSideProps = async () => {
     },
   };
 };
+*/
