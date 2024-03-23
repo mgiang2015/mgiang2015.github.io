c98aefa554056e7c80abd27e2f09880cf8b1e23a
mgiang2015 <mgiang2015@gmail.com>
2024-03-24 00:43:18 +0800

    Update profile to include template nextjs chatbot

---
+++ pages/webweaver.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import WebWeaver from "../components/WebWeaver";
+
+export default function projects() {
+  return (
+    <ContainerBlock title="My Projects">
+      <WebWeaver />
+    </ContainerBlock>
+  );
+}
